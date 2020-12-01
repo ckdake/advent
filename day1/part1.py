ac6dafe9df5fcceeee03ea493ba3ecd29d0b609a
Chris Kelly <ckelly@zenput.com>
2020-12-01 10:57:25 -0500

    Day 1 complete

---
+++ day1/part1.py
@@ -0,0 +1,19 @@
+import csv
+
+# which 2 numbers in input.txt sum to 2020
+
+# what is their product?
+
+numbers = []
+
+with open('input.txt', 'r') as fd:
+  reader = csv.reader(fd)
+  for row in reader:
+    numbers.append(int(row[0]))
+
+for num1 in numbers:
+   for num2 in numbers:
+     if 2020 == num1 + num2:
+        print num1 * num2
+ 	break	
+
