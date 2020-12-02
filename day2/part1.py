e2ace5d40f84b1d50d6df993e53c87ca256ad044
Chris Kelly <ckelly@zenput.com>
2020-12-02 12:43:24 -0500

    Adds working day2

---
+++ day2/part1.py
@@ -0,0 +1,34 @@
+import csv
+
+# for each line, understand the password policy
+
+# how many are passing?
+
+# 1-3 a: abcde
+# 1-3 b: cdefg
+# 2-9 c: ccccccccc
+
+# line 1 means that the password must contain 1,2, or 3 occurances of 'a'
+
+valid_count = 0
+
+with open('input.txt', 'r') as fd:
+  reader = csv.reader(fd)
+  for row in reader:
+    parts =  row[0].split(' ')
+
+    (min_count,max_count) = map(int, parts[0].split('-'))
+    seek_char = parts[1].split(':')[0]
+    candidate_string = parts[2]
+
+    the_count = 0
+
+    for char in list(candidate_string):
+      if char == seek_char: 
+        the_count += 1	
+
+    if the_count >= min_count and the_count <= max_count:
+	valid_count += 1
+
+print valid_count
+
---
+++ day2/part2.py
@@ -0,0 +1,35 @@
+import csv
+
+# for each line, understand the password policy
+
+# how many are passing?
+
+# 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+# 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+# 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+valid_count = 0
+
+with open('input.txt', 'r') as fd:
+  reader = csv.reader(fd)
+  for row in reader:
+    parts =  row[0].split(' ')
+
+    (first_index,second_index) = map(int, parts[0].split('-'))
+    seek_char = parts[1].split(':')[0]
+    candidate_string = parts[2]
+
+    the_count = 0
+
+    candidate_array = list(candidate_string)
+    
+    if candidate_array[first_index - 1] == seek_char:
+	the_count += 1
+    if candidate_array[second_index - 1] == seek_char:
+        the_count += 1
+
+    if the_count == 1: 
+	valid_count += 1
+
+print valid_count
+
